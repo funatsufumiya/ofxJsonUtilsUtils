73a5d739358aa6f531daa34d3b5495d1b428ba90
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2025-09-08 12:51:32 +0900

    workarounds for msvc

--- src/ofxJsonUtilsUtils.h
+++ src/ofxJsonUtilsUtils.h
@@ -17,3 +17,41 @@
 #define JSON_FUNCS(...) \
 void loadJson(const ofJson &json) {ofxJsonUtils::load(json, KV_COMMA_SEPARATION(__VA_ARGS__));}\
 ofJson toJson() const { return ofxJsonUtils::create(KV_COMMA_SEPARATION(__VA_ARGS__));}
+
+// WORKAROUNDS for MSVC below:
+
+#define JSON_FUNCS1(a) \
+void loadJson(const ofJson &json) { ofxJsonUtils::load(json, #a, a); } \
+ofJson toJson() const { return ofxJsonUtils::create(#a, a); }
+
+#define JSON_FUNCS2(a, b) \
+void loadJson(const ofJson &json) { ofxJsonUtils::load(json, #a, a, #b, b); } \
+ofJson toJson() const { return ofxJsonUtils::create(#a, a, #b, b); }
+
+#define JSON_FUNCS3(a, b, c) \
+void loadJson(const ofJson &json) { ofxJsonUtils::load(json, #a, a, #b, b, #c, c); } \
+ofJson toJson() const { return ofxJsonUtils::create(#a, a, #b, b, #c, c); }
+
+#define JSON_FUNCS4(a, b, c, d) \
+void loadJson(const ofJson &json) { ofxJsonUtils::load(json, #a, a, #b, b, #c, c, #d, d); } \
+ofJson toJson() const { return ofxJsonUtils::create(#a, a, #b, b, #c, c, #d, d); }
+
+#define JSON_FUNCS5(a, b, c, d, e) \
+void loadJson(const ofJson &json) { ofxJsonUtils::load(json, #a, a, #b, b, #c, c, #d, d, #e, e); } \
+ofJson toJson() const { return ofxJsonUtils::create(#a, a, #b, b, #c, c, #d, d, #e, e); }
+
+#define JSON_FUNCS6(a, b, c, d, e, f) \
+void loadJson(const ofJson &json) { ofxJsonUtils::load(json, #a, a, #b, b, #c, c, #d, d, #e, e, #f, f); } \
+ofJson toJson() const { return ofxJsonUtils::create(#a, a, #b, b, #c, c, #d, d, #e, e, #f, f); }
+
+#define JSON_FUNCS7(a, b, c, d, e, f, g) \
+void loadJson(const ofJson &json) { ofxJsonUtils::load(json, #a, a, #b, b, #c, c, #d, d, #e, e, #f, f, #g, g); } \
+ofJson toJson() const { return ofxJsonUtils::create(#a, a, #b, b, #c, c, #d, d, #e, e, #f, f, #g, g); }
+
+#define JSON_FUNCS8(a, b, c, d, e, f, g, h) \
+void loadJson(const ofJson &json) { ofxJsonUtils::load(json, #a, a, #b, b, #c, c, #d, d, #e, e, #f, f, #g, g, #h, h); } \
+ofJson toJson() const { return ofxJsonUtils::create(#a, a, #b, b, #c, c, #d, d, #e, e, #f, f, #g, g, #h, h); }
+
+#define JSON_FUNCS9(a, b, c, d, e, f, g, h, i) \
+void loadJson(const ofJson &json) { ofxJsonUtils::load(json, #a, a, #b, b, #c, c, #d, d, #e, e, #f, f, #g, g, #h, h, #i, i); } \
+ofJson toJson() const { return ofxJsonUtils::create(#a, a, #b, b, #c, c, #d, d, #e, e, #f, f, #g, g, #h, h, #i, i); }
