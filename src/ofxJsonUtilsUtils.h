4b38adbbf97fcd8ea967f446122d4b3b918a9671
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2025-09-10 15:21:57 +0900

    feat: #1 up to 14

--- src/ofxJsonUtilsUtils.h
+++ src/ofxJsonUtilsUtils.h
@@ -12,8 +12,13 @@
 #define kv7(name, ...) kv(name), EXPAND(kv6(__VA_ARGS__))
 #define kv8(name, ...) kv(name), EXPAND(kv7(__VA_ARGS__))
 #define kv9(name, ...) kv(name), EXPAND(kv8(__VA_ARGS__))
-#define GET_MACRO(_1,_2,_3,_4,_5,_6,_7,_8,_9,NAME,...) NAME
-#define KV_COMMA_SEPARATION(...) EXPAND(GET_MACRO(__VA_ARGS__, kv9, kv8, kv7, kv6, kv5, kv4, kv3, kv2, kv1)(__VA_ARGS__))
+#define kv10(name, ...) kv(name), EXPAND(kv9(__VA_ARGS__))
+#define kv11(name, ...) kv(name), EXPAND(kv10(__VA_ARGS__))
+#define kv12(name, ...) kv(name), EXPAND(kv11(__VA_ARGS__))
+#define kv13(name, ...) kv(name), EXPAND(kv12(__VA_ARGS__))
+#define kv14(name, ...) kv(name), EXPAND(kv13(__VA_ARGS__))
+#define GET_MACRO(_1,_2,_3,_4,_5,_6,_7,_8,_9,_10,_11,_12,_13,_14,NAME,...) NAME
+#define KV_COMMA_SEPARATION(...) EXPAND(GET_MACRO(__VA_ARGS__, kv14, kv13, kv12, kv11, kv10, kv9, kv8, kv7, kv6, kv5, kv4, kv3, kv2, kv1)(__VA_ARGS__))
 
 #define JSON_FUNCS(...) \
 void loadJson(const ofJson &json) {ofxJsonUtils::load(json, KV_COMMA_SEPARATION(__VA_ARGS__));}\
